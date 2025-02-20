dceeb3e2943708605fedf519ffe6cf439a500060
fullstuckdev <informatikataufik@gmail.com>
2025-02-20 19:22:24 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	utils "struktur-golang/utils"
+)
+func main() {
+
+	utils.Variables()
+	utils.ArrayFunction()
+	utils.Slice()
+	utils.MapFunction()
+	utils.StructFunc()
+}
